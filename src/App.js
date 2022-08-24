6143760bb4917c4c320f44fc193d59d0a72326de
Nathan <salomb3@gmail.com>
2022-08-24 22:50:08 +0100

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Banner } from "./components/Banner";
 import { Projects } from "./components/Projects";
 import { Footer } from "./components/Footer";
 
-
 function App() {
   return (
     <div className="App">
