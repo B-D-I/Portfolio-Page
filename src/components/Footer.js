f40f0eae082754f7efa37e44ffc62fc17e0e7cfc
Nathan <salomb3@gmail.com>
2022-09-04 16:55:30 +0100

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import spaceman from "../assets/img/header-img.svg";
 
 export const Footer = () => {
   return (
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,57 +1,18 @@
-import { useState, useEffect } from "react";
-import {Col, Container, Row} from "react-bootstrap";
+import { useState} from "react";
+import {Container, Row} from "react-bootstrap";
 import 'animate.css';
-import IconJava from 'react-devicon/java/original'
+import hackNeuron from '../assets/pdf/hacking_neurons.pdf';
+import deeplearn from '../assets/pdf/AI_Assign.pdf';
+import datastruct from '../assets/pdf/DataStructures&Algorithms.pdf';
 
 export const Profile = () => {
-  // const [loopNum, setLoopNum] = useState(0);
-  // const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
-  // const [delta, setDelta] = useState(500 - Math.random() * 300);
-  // const [index, setIndex] = useState(1);
-  // const toRotate = [ "Nathan Hewett: Portfolio" ];
-  // const period = 3000;
-
-  // useEffect(() => {
-  //   let ticker = setInterval(() => {
-  //     tick();
-  //   }, delta);
-  //
-  //   return () => { clearInterval(ticker) };
-  // }, [text])
-  //
-  // const tick = () => {
-  //   let i = loopNum % toRotate.length;
-  //   let fullText = toRotate[i];
-  //   let updatedText = isDeleting ? fullText.substring(0, text.length - 2) : fullText.substring(0, text.length + 2);
-  //
-  //   setText(updatedText);
-  //
-  //   if (isDeleting) {
-  //     setDelta(prevDelta => prevDelta / 2);
-  //   }
-  //
-  //   if (!isDeleting && updatedText === fullText) {
-  //     setIsDeleting(true);
-  //     setIndex(prevIndex => prevIndex - 1);
-  //     setDelta(period);
-  //   } else if (isDeleting && updatedText === '') {
-  //     setIsDeleting(false);
-  //     setLoopNum(loopNum + 1);
-  //     setIndex(1);
-  //     setDelta(500);
-  //   } else {
-  //     setIndex(prevIndex => prevIndex + 1);
-  //   }
-  // }
 
   return (
     <section className="banner" id="home">
       <Container>
         <Row className="align-items-center">
-          {/*<Col xs={13} md={7} xl={8}>*/}
-          {/*  <h1> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Nathan Hewett: Portfolio" ]'><span className="wrap">{text}</span></span></h1>*/}
-          <div id="text_area">
+            <div id="text_area">
           <h1>Nathan Hewett: Portfolio</h1>
           <p id="profile" >
                   I am a graduate software engineer and have developed various applications including;
@@ -60,33 +21,13 @@ export const Profile = () => {
                   TestNG, JUnit and Selenium, and have gained knowledge of concepts including;
                   object-orientated programming, data structures & algorithms, data analytics, and cyber security.
                   <br /><br />
-                  Example deep learning project:<br />
-                  Covid X-Ray Image Classification: Comparing A Convolution Neural Network And Support Vector Machine:
-                  <a href="https://colab.research.google.com/drive/1njoVp-RxlcpvlyWVjODnNblqSH6oE9Vo?usp=sharing"> Link</a>
+              <h2>Reports:</h2>
+                <li><a href={hackNeuron} target="_blank">Hacking Neurons: An analysis of deep learning vulnerabilities</a></li>
+                <li><a href={deeplearn} target="_blank">Deep Learning: Image Classification using a Convolutional Neural Network and Support Vector
+                    Machine </a></li>
+                <li><a href={datastruct} target="_blank">Data Structures & Algorithms: Flight Scheduling Application </a></li>
                 </p>
-
           </div>
-          {/*</Col>*/}
-
-          {/*<Col id="languages" xs={10} md={4} xl={3} className="tagline">*/}
-          {/*      <br />*/}
-          {/*        <li>*/}
-          {/*          Java*/}
-          {/*        </li>*/}
-          {/*         <li>*/}
-          {/*          Python*/}
-          {/*        </li>*/}
-          {/*         <li>*/}
-          {/*          JavaScript*/}
-          {/*        </li>*/}
-          {/*         <li>*/}
-          {/*          PHP*/}
-          {/*        </li>*/}
-          {/*          <li>*/}
-          {/*          C++*/}
-          {/*        </li>*/}
-          {/*    <br />*/}
-          {/*</Col>*/}
         </Row>
       </Container>
     </section>
